1e2e381f0658e39eabead69f4a93ce1bb3b3f414
Dima Kuzko <bigmandarine1991@gmail.com>
2022-11-22 17:02:07 +0200

    проблема с localStorage
    При обновлении страницы, данные в localStorage очищаются.

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,65 +1,64 @@
-import React from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import { Form } from './ContactForm.styled';
-class ContactForm extends React.Component {
-  state = {
-    name: '',
-    number: '',
-  };
-  nameId = nanoid();
-  phoneId = nanoid();
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const nameId = nanoid();
+  const phoneId = nanoid();
 
-  onHandleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value });
+  const onNameChange = evt => {
+    setName(evt.target.value);
   };
-
-  reset = () => {
-    this.setState({ name: '', number: '' });
+  const onNumberChange = evt => {
+    setNumber(evt.target.value);
   };
 
-  handleSubmit = evt => {
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  const handleSubmit = evt => {
     evt.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
   };
 
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            id={this.nameId}
-            value={this.state.name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            onChange={this.onHandleChange}
-            required
-          />
-        </label>
-        <label>
-          Number
-          <input
-            type="tel"
-            name="number"
-            id={this.phoneId}
-            value={this.state.number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            onChange={this.onHandleChange}
-            required
-          />
-        </label>
-        <button type="submit">Add contact</button>
-      </Form>
-    );
-  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input
+          type="text"
+          name="name"
+          id={nameId}
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          onChange={onNameChange}
+          required
+        />
+      </label>
+      <label>
+        Number
+        <input
+          type="tel"
+          name="number"
+          id={phoneId}
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          onChange={onNumberChange}
+          required
+        />
+      </label>
+      <button type="submit">Add contact</button>
+    </Form>
+  );
 }
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
-export default ContactForm;
